a04f366551ea52cf9ec71a8ba5930126e0db6e40
HarryStevens <harryjosephstevens@gmail.com>
2014-03-06 12:28:31 -0500

    fusion table loaded with console log confirming

--- scripts.js
+++ scripts.js
@@ -11,14 +11,19 @@
  */
 
 //1. Document ready will call pageReady function
-$(document).ready(pageReady);
+$(document).ready(MpageReady);
 
 //2. pageReady funciton will load the Google visualization library and call googleReady function
-function pageReady(){
-	google.load("visualization", "1", {packages:["corechart"],callback:googleReady});
+function MpageReady(){
+	google.load("visualization", "1", {packages:["corechart"],callback:MgoogleReady});
 }
 
 //3. googleReady function will load the json from the fusion table and call dateReady function
-function googleReady(){
-	
+function MgoogleReady(){
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1LOmK0eFCAtFjzFW-kVx7dlbRcIJNSEC4wN7SD-da&key=AIzaSyB-QJux9WIJmey5IJYzPImNzg-xP1gpvU8",MdataReady,"json");
+}
+
+//4. dataReady function will format the data, feed it into the Google visualization library, and display it on the page
+function MdataReady(GAS){
+	console.log(GAS);
 }
